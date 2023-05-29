ed7df96af05263554251d773dd315ea845312c54
Andrew <andrew12.hk@gmail.com>
2023-05-29 17:56:09 +0300

    firefox broken, janus updated but not fixed the firefox connection

--- media-chat/common/js/vendor/janus.js
+++ media-chat/common/js/vendor/janus.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:336bd007c504d2a80ae61fb1e9b7f41a2cd2a3071ff7e5776c03c55b4711f85c
-size 110066
+oid sha256:7a1a792fa1b2574766e3fccebbe5cc5ad0d40b7f3a9b3c61e01be1f42d2593e8
+size 113348
