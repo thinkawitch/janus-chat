783d81453197d4af33369503c6f981426d5dafbb
Andrew <andrew12.hk@gmail.com>
2023-06-09 16:54:41 +0300

    update to symfony 6.3

--- media-chat/api/src/Serializer/MyCustomProblemNormalizer.php
+++ media-chat/api/src/Serializer/MyCustomProblemNormalizer.php
@@ -6,20 +6,29 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class MyCustomProblemNormalizer implements NormalizerInterface
 {
-    public function normalize($exception, string $format = null, array $context = [])
+    public function normalize(mixed $object, string $format = null, array $context = []) : array|string|int|float|bool|\ArrayObject|null
     {
+        if (!$object instanceof FlattenException) {
+            throw new \Exception(sprintf('The object must implement "%s".', FlattenException::class));
+        }
+
         return [
             'content' => 'This is my custom problem normalizer.',
             'exception'=> [
-                'message' => $exception->getMessage(),
-                'code' => $exception->getStatusCode(),
+                'message' => $object->getMessage(),
+                'code' => $object->getStatusCode(),
             ],
         ];
     }
 
-    public function supportsNormalization($data, string $format = null, array $context = [])
+    public function supportsNormalization(mixed $data, string $format = null, array $context = []) : bool
     {
         return false; //turn off
         return $data instanceof FlattenException;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return ['*' => null]; //supports nothing
     }
 }
